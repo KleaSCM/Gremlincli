56e7eb31787899bc06afec08208c27cbaa521810
KleaSCM <kleascm@gmail.com>
2025-04-18 13:22:21 +0000

    Remove unused subprojects and their associated files: akhsrbfk, gremlincli, testies, and awe. This cleanup enhances project organization and reduces clutter.

--- gremlincli/target/debug/deps/gremlincli-142efd1de18aadc6.d
+++
@@ -1,5 +0,0 @@
-/home/klea/Documents/Dev/Gremlincli/gremlincli/target/debug/deps/gremlincli-142efd1de18aadc6: src/main.rs
-
-/home/klea/Documents/Dev/Gremlincli/gremlincli/target/debug/deps/gremlincli-142efd1de18aadc6.d: src/main.rs
-
-src/main.rs:
--- gremlincli/target/debug/gremlincli.d
+++
@@ -1,1 +0,0 @@
-/home/klea/Documents/Dev/Gremlincli/gremlincli/target/debug/gremlincli: /home/klea/Documents/Dev/Gremlincli/gremlincli/src/main.rs
